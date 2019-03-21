49c68b95c22337d7be14c4f69ab57790bddba8c4
Pasha <vfaryvo@mail.ru>
2019-03-21 16:06:00 +0500

    Added animal

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Menu from "./Menu/Menu";
+import Card from "./Card/Card";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Menu/>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <header>
+                    <Menu/>
+                </header>
+                <section id="main">
+                    <div className="container">
+                        <h2>Найдите любимца в нашем Питомнике!</h2>
+                        <Card/>
+                    </div>
+                </section>
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/Card/Card.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import img from '../images/sample-card-image.jpg'
+import './Card.css'
+
+const Card = ({src = img, name = 'Sample', text = 'Очень ласковый и любвеобильный котик.', sex = 'male', age = "12"}) => {
+    return (
+        <a href="#">
+            <div className="card">
+                <img src={img} alt="animal" className='animal'/>
+                <div className="text-wrapper">
+                    <h3 className='animal-name'>{name}</h3>
+                    <p className='animal-text'>{trimText(text)}</p>
+                    <div className="animal-data" style={{color: colorText(sex)}}>
+                        {makeSex(sex)}
+                    </div>
+                </div>
+            </div>
+        </a>
+
+    )
+};
+const makeSex = (sex) => {
+    const sexIcon = sex === 'male'
+        ? <i className="fa fa-mars" aria-hidden="true"/>
+        : <i className="fa fa-venus" aria-hidden="true"/>;
+    return sexIcon
+
+};
+const colorText = (sex) => {
+    return sex === 'male' ? '#2196f3' : '#f06292'
+}
+const trimText = (text, value = 40) => {
+    return text.length > value ? text.slice(0, value - 3) + '...' : text;
+
+
+}
+export default Card;
+export {Card};
