98f15132b29272370e0ab2b989e8ee87dc1513b6
Pasha <vfaryvo@mail.ru>
2019-03-21 13:32:49 +0500

    Added menu

---
+++ src/Menu/Menu.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './Menu.css'
+import logo from '../images/Питомник.svg'
+
+const Menu = () => {
+    return (
+        <div className="menu">
+            <div className="container ">
+                <div className="menu-container">
+                    <a href="#"><img src={logo} alt="logo" className="logo"/></a>
+                    <nav>
+                        <div className="menu-element"><a href="#">Главная</a></div>
+                        <div className="menu-element"><a href="#">О нас</a></div>
+                        <div className="menu-element"><a href="#">Правила</a></div>
+                    </nav>
+                    <div className="number">+7 900 000 00 00</div>
+                </div>
+            </div>
+        </div>
+    )
+};
+export default Menu
+export {Menu}
